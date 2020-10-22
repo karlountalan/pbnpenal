45dd221a64d625193dd26d6fe368f8c8f870f3b1
Karlo Untalan <untalankarlo@gmail.com>
2020-10-22 16:25:35 +0800

    allowed Host

--- google_check/settings.py
+++ google_check/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ko=&($15$*70!&)=$+-$3z%-3@howjy!=4ykx9h8)u(9kj!t&&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['162.243.13.110']
 
 
 # Application definition
