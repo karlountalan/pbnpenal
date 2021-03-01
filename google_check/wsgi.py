a1947028f6afe4370d29df44262731fff0092c57
Karlo Untalan <karlountalan@Karlos-MacBook-Pro.local>
2021-03-01 21:00:37 +0800

    Proxy and Deployment

--- google_check/wsgi.py
+++ google_check/wsgi.py
@@ -7,14 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google_check.settings')
-
-application = get_wsgi_application()
-
 
 import os
 import sys #Add this
