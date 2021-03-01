419dfa3e4af630145591e4d784007ff8f88c0c38
Karlo Untalan <karlountalan@Karlos-MacBook-Pro.local>
2021-03-02 01:01:07 +0800

    Proxy and Deployment

--- google_check/settings.py
+++ google_check/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'google_check.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'testing1_django',
-'USER': 'testing1_django',
-'PASSWORD': 'WNm9nZ3rqDwquQajeBZr',
-'HOST': '127.0.0.1',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-}
+
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.mysql',
+# 'NAME': 'testing1_django',
+# 'USER': 'testing1_django',
+# 'PASSWORD': 'WNm9nZ3rqDwquQajeBZr',
+# 'HOST': '127.0.0.1',
+# }
+# }
 
 
 # Password validation
